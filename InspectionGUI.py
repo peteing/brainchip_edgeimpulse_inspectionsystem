39a69d5daff974c129d7ed5d56720eaeb8326b8c
Peter Ing <peter.ing@outlook.com>
2024-02-07 16:43:57 +0200

    fg

--- InspectionGUI.py
+++ InspectionGUI.py
@@ -58,14 +58,14 @@ class VideoDisplay(QLabel):
     def inspect_frame(self, frame):
         global counter
         # This is where we perform the inference in case you are looking for it
-        
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         height, width, _ = frame_rgb.shape
         print("Height" + str(height) + "Width" + str(width))
 
         offset = abs(width -height)/2
         print(offset)
         input_frame = cv2.resize(frame_rgb,(akida_model_objectdet_inshape[0],akida_model_objectdet_inshape[1]) ) # We need to resize the frame to the input layer of the Akida
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
         input_frame_akida = np.expand_dims(input_frame, axis=0) # this needed to create the correct input tesnsor for the Akida which includes a batch size of 1 in this case
         counter = counter + 1
 
